7e641a67a6c73205e235b449aa80d9b4dd18acfb
SKYRIMpro1 <stephen12stewart@gmail.com>
2022-02-20 20:06:10 -0600

    Initial Questions and buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,26 @@
 var startBtn=document.getElementById('startBtn');
+var correctBtn=document.getElementById('correctBtn');
 var landingPg=document.getElementById('landingPg');
-var Q1=document.getElementById('Q1')
+var Q1=document.getElementById('Q1');
+var Q2=document.getElementById('Q2');
+var End=document.getElementById('End');
 console.log(startBtn);
 
 startBtn.addEventListener('click', function(){
     console.log('clicked')
     landingPg.classList.add('hidden')
     Q1.classList.remove('hidden')
+})
+
+correctBtn.addEventListener('click', function(){
+    console.log('clicked')
+    Q1.classList.add('hidden')
+    Q2.classList.remove('hidden')
+})
+
+wrongBtn.addEventListener('click', function(){
+    console.log('clicked')
+    Q1.classList.add('hidden')
+    End.classList.remove('hidden')
 })
 
