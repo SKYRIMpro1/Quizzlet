2c5375041c6b9944b2cb0bd2f4589c7efb752322
SKYRIMpro1 <stephen12stewart@gmail.com>
2022-02-20 20:25:26 -0600

    Comments

--- Assets/script.js
+++ Assets/script.js
@@ -4,20 +4,23 @@ var landingPg=document.getElementById('landingPg');
 var Q1=document.getElementById('Q1');
 var Q2=document.getElementById('Q2');
 var End=document.getElementById('End');
-console.log(startBtn);
 
+
+//Start button on launching page
 startBtn.addEventListener('click', function(){
     console.log('clicked')
     landingPg.classList.add('hidden')
     Q1.classList.remove('hidden')
 })
 
+//Correct button will send you to next page
 correctBtn.addEventListener('click', function(){
     console.log('clicked')
     Q1.classList.add('hidden')
     Q2.classList.remove('hidden')
 })
 
+//Wrong button will send you to logging page and restart button
 wrongBtn.addEventListener('click', function(){
     console.log('clicked')
     Q1.classList.add('hidden')
